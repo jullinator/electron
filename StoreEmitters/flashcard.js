7c0b91e6fb01699b828796c944e8a875f28ddee4
jullinator <justus.karlsson@hotmail.se>
2016-09-16 01:49:00 +0200

    Update flashcard.js

--- StoreEmitters/flashcard.js
+++ StoreEmitters/flashcard.js
@@ -7,12 +7,26 @@ const SCHEMA = {
   
 }
 
+var EventEmitter = require('events')
+
+class onFlashcard extends EventEmitter {
+  constructor(){
+    super()
+    
+  }
+  reschedule(){ }
+}
+
 
 //save --> doc = { @SCHEMA }
 db.insert(doc, function(err, newDoc){
   
 })
 
+// e:{ $regex: /kon/, $nin: ['konnichiwa'] } 
+  //-->  $in, $gt:(str||int) == $lt , 
+ // $exists:true, 
+ //$or:[q1, q2] ==/= $and,   ($not:{a:'a'} ==/== a:'a')
 db.find({ expression: /kon/ }, function(err, docs){
   // konnichiwa, kon, 
 })
