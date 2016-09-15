e12d19a51d5dac7546d125919dc278c9db6c3073
jullinator <justus.karlsson@hotmail.se>
2016-09-16 01:38:52 +0200

    Update flashcard.js

--- StoreEmitters/flashcard.js
+++ StoreEmitters/flashcard.js
@@ -1,2 +1,23 @@
+//  FLASHCARD.JS
 
 const SCHEMA = {
+  expression: String,
+  meaning: String,
+  created: Date,
+  
+}
+
+
+//save --> doc = { @SCHEMA }
+db.insert(doc, function(err, newDoc){
+  
+})
+
+db.find({ expression: /kon/ }, function(err, docs){
+  // konnichiwa, kon, 
+})
+
+let flashId = 'ajs754729f'
+db.findOne({_id: flashId}, function(err, doc){
+  
+})
